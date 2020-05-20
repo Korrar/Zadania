d9c7424e3ac3100ad6f61a59f5b84a53b041b1e3
unknown <korrarkk@gmail.com>
2020-05-20 05:07:25 +0200

    done

--- Dictionary.py
+++ Dictionary.py
@@ -7,7 +7,7 @@ class LinkedList:
     def __getitem__(self, item):
         i = 0
         if self.head == self.tail:
-            return self.head
+            return self.head.data
         trav = self.head
         while trav != self.tail:
             if i == item:
@@ -37,18 +37,23 @@ class LinkedList:
         self.size = self.size +1
 
 
-
+def hashi(key):
+    ha = 0
+    for h in key:
+        ha = ha + ord(h)
+    return ha % 256
 
 class Tup:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.hash = hash(key)
+        self.hash = hashi(key)
+
 
 class HashMap:
     def __init__(self):
-        self.default_size = 3
-        self.table = LinkedList()
+        self.default_size = 256
+        self.table = [None] * self.default_size
 
     def add(self, key, value):
         entry = Tup(key, value)
@@ -56,14 +61,28 @@ class HashMap:
         return self.insert(entry, index)
 
     def insert(self, entry, index):
-        bucket = self.table[index]
+
+        if self.table[index] is None:
+            self.table[index] = LinkedList()
+        self.table[index].append(entry)
+
+    def get(self, key):
+        key_str = key
+        index = hashi(key)
+        i = 0
+        for key in self.table[index]:
+            if self.table[index][i].key == key_str:
+                print(self.table[index][i].value)
+                break
+            i = i+1
 
 
 
 
-do = LinkedList()
-do.append('adsdasdad')
-do.append('ad33sdasdad')
-print(do[1])
+do = HashMap()
+do.add('a','hello')
+do.add('v','he222o')
+do.get('v')
+do.get('a')
 
 
